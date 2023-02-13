330c9232a1db83c78b1dfe0f9ce15fc78704bdeb
curtlen88 <curtlen88@gmail.com>
2023-02-13 13:23:02 -0700

    navbar made

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import {Link} from 'react-router-dom';
+
+export default function Navbar(currentUser, handleLogout) {
+    const loggedIn = (
+        <>
+            <Link to="/">🛹FOAS</Link>
+            <br/>
+            <Link to="/profile">Profile</Link>
+            <br/>
+            <Link to="/logout">Logout</Link>
+            <br/>
+        </>
+    )
+
+    const loggedOut = (
+        <>
+            <Link to="/">🛹FOAS</Link>
+            <br/>
+            <Link to="/login">Login</Link>
+            <br/>
+            <Link to="/signup">Signup</Link>
+            <br/>
+        </>
+    )
+
+    return (
+        <>
+            {currentUser ? loggedIn : loggedOut}
+        </>
+    )
+}
