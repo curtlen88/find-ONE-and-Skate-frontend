44a77b81e1e60f557aa34a0146fab65875dfd738
curtlen88 <curtlen88@gmail.com>
2023-02-13 14:37:41 -0700

    reorganized pages working on user auth

--- src/App.js
+++ src/App.js
@@ -2,31 +2,94 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate
-} from 'react-router-dom'
+} from "react-router-dom";
 
+import { useState, useEffect } from "react";
+import jwt_decode from "jwt-decode";
 
 // component imports
-import Map from "./components/Map";
+import Home from "./components/pages/Home";
+import Register from "./components/pages/Register";
+import Login from "./components/pages/Login";
+import Profile from "./components/pages/Profile";
 import Navbar from "./components/Navbar";
-
+import Welcome from "./components/pages/Welcome";
 
 export default function App() {
+  // the currently logged in user will be stored up here in state
+  const [currentUser, setCurrentUser] = useState(null);
 
+  // useEffect -- if the user navigates away form the page, we will log them back in
+  useEffect(() => {
+    // check to see if token is in storage
+    const token = localStorage.getItem("jwt");
+    if (token) {
+      // if so, we will decode it and set the user in app state
+      setCurrentUser(jwt_decode(token));
+    } else {
+      setCurrentUser(null);
+    }
+  }, []); // happen only once
+
+  // event handler to log the user out when needed
+  const handleLogout = () => {
+    // check to see if a token exists in local storage
+    if (localStorage.getItem("jwt")) {
+      // if so, delete it
+      localStorage.removeItem("jwt");
+      // set the user in the App state to be null
+      setCurrentUser(null);
+    }
+  };
 
   return (
     <div>
-        <Router>
-          <header>
-            <Navbar />
-          </header>
-          <div className='map'>
-            <main>
-              <Map />
-            </main>
-          </div>
-        </Router>
-      </div>
+      <Router>
+        <header>
+          <Navbar currentUser={currentUser} handleLogout={handleLogout} />
+        </header>
+        <div className="App">
+          <Routes>
+            <Route 
+              path="/" 
+              element={<Home />} 
+            />
+            <Route 
+              path="/welcome"
+              element={<Welcome />}
+            />
+            <Route
+              path="/users/register"
+              element={
+                <Register
+                  currentUser={currentUser}
+                  setCurrentUser={setCurrentUser}
+                />
+              }
+            />
+            <Route
+              path="/users/login"
+              element={
+                <Login
+                  currentUser={currentUser}
+                  setCurrentUser={setCurrentUser}
+                />
+              }
+            />
 
+            <Route
+              path="/users/:id"
+              element={
+                <Profile
+                  handleLogout={handleLogout}
+                  currentUser={currentUser}
+                  setCurrentUser={setCurrentUser}
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
+    </div>
   );
 }
