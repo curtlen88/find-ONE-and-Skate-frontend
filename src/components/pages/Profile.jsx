8e8219eaec755b06f178b5d725823bb3527e42c9
curtlen88 <curtlen88@gmail.com>
2023-02-13 17:52:07 -0700

    dbspots rendering, saves when you click on dropped point  but erases the previous db point

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -1,10 +1,10 @@
-import { useState, useEffect } from 'react'
-import { Navigate } from 'react'
-import axios from 'axios'
-import Login from './Login'
+// import { useState, useEffect } from 'react'
+// import { Navigate } from 'react'
+// import axios from 'axios'
+// import Login from './Login'
 
 export default function Profile(props) {
-  const [message, setMessage] = useState('')
+  // const [message, setMessage] = useState('')
 
   
   return (
