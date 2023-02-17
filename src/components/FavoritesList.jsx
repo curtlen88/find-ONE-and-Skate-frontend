e8e56aa6f345e606c126cc8d15e698bc4947cc82
curtlen88 <curtlen88@gmail.com>
2023-02-16 22:12:43 -0700

    almost there

--- src/components/FavoritesList.jsx
+++ src/components/FavoritesList.jsx
@@ -1,44 +1,60 @@
-import { clearStorage } from 'mapbox-gl'
 import { useState, useEffect } from 'react'
-// import axios from 'axios'
-// import User from '../models/User'
-// import Spot from '../../../foas-backend/models/Spot'
-
+import axios from 'axios'
+import decode from 'jwt-decode'
 
 export default function FavoritesList(props) {
 
-    // const [favorites, setFavorites] = useState([])
-    // const token = localStorage.getItem('jwt')
+    const [favorites, setFavorites] = useState([])
+    const userId = decode(localStorage.getItem('jwtToken')).id
     
-    // GET /favorites 
-    useEffect(() => {
-        // const getFavorites = async () => {
-        //     const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/favorites`, {headers: {Authorization: token} })
-        //     console.log(response.data.favSpots)
-        //     setFavorites(response.data.favSpots)
-        //     // map over favorites and display them
-        // }
-        // getFavorites()
-        // console.log("props.currentUser -----", props.currentUser)
-        // const usersSpots = props.currentUser.populate('favSpots')
+    useEffect(()  => {
+        const fetchFavorites = async () => {
+        try {
+        // get the userFavorites from the backend
+        const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/users/${userId}`)
+        // set the favorites state to the userFavorites
+        console.log(response.data.favSpots[0].images[0])
+        console.log(response.data.favSpots)
 
-        // console.log(usersSpots)
+        setFavorites(response.data.favSpots)
+        } catch (error) {
+            console.log(error)
+        }
+        }
+        fetchFavorites()
     }, [])
-    
 
-    // const listFavorites = favorites.map(favorite => {
-    //     return (
-    //         <div>
-    //             <h3>{favorite.name}</h3>
-    //             <p>{favorite.description}</p>
-    //             <img src={favorite.image[0]} alt={favorite.name} />
-    //         </div>
-    //     )
-    // })
 
-    return (
-        <div>
-            {/* {listFavorites} */}
-        </div>
-    )
+    if (favorites.length > 0) {
+    const listFavorites = favorites.map(favorite => {
+        return (
+            <div className='card=container'>
+                <div className='card' key={favorite._id}>
+                    <img className='favImg' src={favorite.images[0]} alt={favorite.name}/>
+                    < div className='card-text'>
+                        <h2>{favorite.name}</h2>
+                        <p>{favorite.description}</p>
+                    </div>
+                </div>
+            </div>
+        )
+    })
+        return (
+            <div>
+                <div id="favHeader">
+                    <h1>My Favorites</h1>
+                    <div class="skateboard-shape">
+                    </div>
+                </div>
+                {listFavorites}
+            </div>
+        )
+    } else {
+        return (
+            <div className='card-text'>
+                <h2>You have no favorites</h2>
+            </div>
+        )
+    }
+
 }
